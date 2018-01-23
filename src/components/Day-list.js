bcc60342f11de0d9242afc4fdb583a2fbb558b14
Fernando Fragoso <fernandofragoso@gmail.com>
2018-01-23 01:05:00 -0300

    Loading state

--- src/components/Day-list.js
+++ src/components/Day-list.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import Day from './Day';
 
 export default class DayList extends Component {
+
   render() {
+    let days = this.props.days.map(day => <Day onUpdateDay={this._updateDay.bind(this)} date={day.date} hours={day.hours} id={day.key} key={day.key} />);
+    if (this.props.days.length === 0) {
+      days = <div className='loading'>Loading...</div>
+    }
+
     return (
-      this.props.days.map(day => <Day onUpdateDay={this._updateDay.bind(this)} date={day.date} hours={day.hours} id={day.key} key={day.key} />)
+      days
     );
   }
 
